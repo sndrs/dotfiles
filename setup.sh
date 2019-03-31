d83afc313706c55656c69bc0082bf8470850e30f
Alex Sanders <a@snd.rs>
2019-03-31 23:43:23 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Update all system software
+sudo softwareupdate -i -a
+
+# Install xcode commandline tools
+xcode-select --install
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/latest/install.sh | bash
